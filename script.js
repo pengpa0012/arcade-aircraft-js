5137c0edb9fe12fc515001cbf377d1c920e48387
Pengpa0012 <peng.ilikha@gmail.com>
2024-05-07 20:48:15 +0800

    added initial player

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-kaboom()
-add([
-    text("hello"),
-    pos(120, 80),
+kaboom({
+    background: [ 255, 255, 255, ]
+})
+
+loadSprite("player", "./assets/aircraft.png")
+loadSprite("bullet", "./assets/bullet.png")
+
+const player = add([
+    sprite("player"),
+    pos(width() / 2, height() - 100),
 ])
+
+player.onUpdate(() => {
+    if (isKeyDown("w")) {
+        player.move(0, -300)
+    }
+    if (isKeyDown("a")) {
+        player.move(-300, 0)
+    }
+    if (isKeyDown("s")) {
+        player.move(0, 300)
+    }
+    if (isKeyDown("d")) {
+        player.move(300, 0)
+    }
+})
