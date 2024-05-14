3ae49f79dc7677a5c56396a61f98481c8bc1629e
Pengpa0012 <peng.ilikha@gmail.com>
2024-05-14 14:48:27 +0800

    added initial menu/score screen

--- script.js
+++ script.js
@@ -10,194 +10,247 @@ loadSprite("ammo", "./assets/images/ammo.png")
 loadSound("shoot", "./assets/sfx/shoot.mp3")
 loadSound("hurt", "./assets/sfx/hurt.mp3")
 loadSound("explode", "./assets/sfx/explode.ogg")
+let isGameStart = false
 
-const player = add([
-    sprite("player"),
-    pos(width() / 2, height() - 100),
-    area(),
-	health(5),
-    "player"
-])
-
-const score = add([
-    text("Score: 0"),
-    color(0, 0, 0),
-    pos(50, 50),
-	z(100),
-    { value: 0 },
-])
-const life = add([
-    text("Life: 5"),
-    color(0, 0, 0),
-    pos(50, 100),
-	z(100),
-    { value: player.hp() },
-])
-
-function spawnPowerUps() {
-    const randomize = rand(0, 5)
-    console.log(randomize)
+function startMenu() {
     add([
-        sprite(randomize > 1 ? "ammo" : "heart"),
-        pos(rand(0, width() - 100), -100),
+        text("Start Game"),
+        pos((width() - 202.5) / 2, height() / 2),
+        color(0,0,0),
         area(),
-        "power_ups"
+        "start"
     ])
+    isGameStart = false
 }
 
-onUpdate("power_ups", (power_ups) => {
-    if(power_ups.pos.y >= height()) {
-		destroy(power_ups)
-	}
-	power_ups.move(0, 80)
+startMenu()
+
+onClick("start", (start) => {
+    destroy(start)
+    destroyAll("final_score")
+    isGameStart = true
+    startGame()
 })
 
-function spawnEnemy() {
+function scoreMenu(score) {
     add([
-        sprite("enemy"),
-        pos(rand(0, width() - 100), -100),
+        text(`Your Score: ${score}`),
+        pos((width() - 283) / 2, (height() - 100) / 2),
+        color(0,0,0),
         area(),
-        "enemy"
+        "final_score"
     ])
-    const ticker = 3 - (score.value / 100)
-    wait(ticker < 1 ? 1 : ticker, spawnEnemy)
+    startMenu()
 }
 
-spawnEnemy()
-
-onUpdate("enemy", (enemy) => {
-	if(enemy.pos.y >= height()) {
-		destroy(enemy)
-	}
-	enemy.move(0, 100)
-})
-
-function spawnEnemyBullet() {
-    // if(player.hp() <= 0) return
-	const enemies = get("enemy")
-
-	enemies.forEach(enemy => {
-		add([
-			sprite("bullet"),
-			pos(enemy.pos.x + 40, enemy.pos.y + enemy.height),
-			area(),
-			"enemy_bullet"
-		])
-	})
-	wait(2.5, spawnEnemyBullet)
-}
-
-spawnEnemyBullet()
-
-onUpdate("enemy_bullet", (enemy_bullet) => {
-	if(enemy_bullet.pos.y >= height()) {
-		destroy(enemy_bullet)
-	}
-	enemy_bullet.move(0, 500)
-})
-
-function spawnBullet() {
-    // if(player.hp() <= 0) return
-	add([
-        sprite("bullet"),
-        pos(player.pos.x + 32, player.pos.y),
-		area(),
-		"bullet"
+function startGame() {
+    if(!isGameStart) return
+    const player = add([
+        sprite("player"),
+        pos(width() / 2, height() - 100),
+        area(),
+        health(5),
+        "player"
     ])
-    play("shoot", {volume: 0.1})
-    if(player.hp() > 0) {
-        const ticker = 1 - (score.value / 1000)
-        wait(ticker < 0.09 ? 0.09 : ticker, spawnBullet)
+    
+    const score = add([
+        text("Score: 0"),
+        color(0, 0, 0),
+        pos(50, 50),
+        z(100),
+        { value: 0 },
+    ])
+    const life = add([
+        text("Life: 5"),
+        color(0, 0, 0),
+        pos(50, 100),
+        z(100),
+        { value: player.hp() },
+    ])
+    
+    function spawnPowerUps() {
+        if(!isGameStart) return
+        const randomize = rand(0, 5)
+        add([
+            sprite(randomize > 1 ? "ammo" : "heart"),
+            pos(rand(0, width() - 100), -100),
+            area(),
+            "power_ups"
+        ])
     }
-}
-
-spawnBullet()
-
-onUpdate("bullet", (bullet) => {
-	bullet.move(0, -1000)
-	if(bullet.pos.y <= -100) {
-		destroy(bullet)
-	}
-})
-
-player.onUpdate(() => {
-    // if(player.hp() <= 0) return
-
-    // move
-    if (isKeyDown("w")) {
-        if(player.pos.y <= 0) {
-            player.move(0, 0)
-        } else {
-            player.move(0, -500)
+    
+    onUpdate("power_ups", (power_ups) => {
+        if(power_ups.pos.y >= height()) {
+            destroy(power_ups)
+        }
+        power_ups.move(0, 80)
+    })
+    
+    function spawnEnemy() {
+        if(!isGameStart) return
+        add([
+            sprite("enemy"),
+            pos(rand(0, width() - 100), -100),
+            area(),
+            "enemy"
+        ])
+        const ticker = 3 - (score.value / 100)
+        wait(ticker < 1 ? 1 : ticker, spawnEnemy)
+    }
+    
+    spawnEnemy()
+    
+    onUpdate("enemy", (enemy) => {
+        if(enemy.pos.y >= height()) {
+            destroy(enemy)
+        }
+        enemy.move(0, 100)
+    })
+    
+    function spawnEnemyBullet() {
+        if(!isGameStart) return
+        // if(player.hp() <= 0) return
+        const enemies = get("enemy")
+    
+        enemies.forEach(enemy => {
+            add([
+                sprite("bullet"),
+                pos(enemy.pos.x + 40, enemy.pos.y + enemy.height),
+                area(),
+                "enemy_bullet"
+            ])
+        })
+        wait(2.5, spawnEnemyBullet)
+    }
+    
+    spawnEnemyBullet()
+    
+    onUpdate("enemy_bullet", (enemy_bullet) => {
+        if(enemy_bullet.pos.y >= height()) {
+            destroy(enemy_bullet)
+        }
+        enemy_bullet.move(0, 500)
+    })
+    
+    function spawnBullet() {
+        if(!isGameStart) return
+        // if(player.hp() <= 0) return
+        add([
+            sprite("bullet"),
+            pos(player.pos.x + 32, player.pos.y),
+            area(),
+            "bullet"
+        ])
+        play("shoot", {volume: 0.1})
+        if(player.hp() > 0) {
+            const ticker = 1 - (score.value / 1000)
+            wait(ticker < 0.09 ? 0.09 : ticker, spawnBullet)
         }
     }
-    if (isKeyDown("a")) {
-        if(player.pos.x <= 0) {
-            player.move(0, 0)
-        } else {
-            player.move(-500, 0)
+    
+    spawnBullet()
+    
+    onUpdate("bullet", (bullet) => {
+        bullet.move(0, -1000)
+        if(bullet.pos.y <= -100) {
+            destroy(bullet)
         }
-    }
-    if (isKeyDown("s")) {
-        if(player.pos.y >= height() - player.height) {
-            player.move(0, 0)
-        } else {
-            player.move(0, 500)
+    })
+    
+    player.onUpdate(() => {
+        // if(player.hp() <= 0) return
+    
+        // move
+        if (isKeyDown("w")) {
+            if(player.pos.y <= 0) {
+                player.move(0, 0)
+            } else {
+                player.move(0, -500)
+            }
         }
-    }
-    if (isKeyDown("d")) {
-        if(player.pos.x >= width() - player.width) {
-            player.move(0, 0)
-        } else {
-            player.move(500, 0)
+        if (isKeyDown("a")) {
+            if(player.pos.x <= 0) {
+                player.move(0, 0)
+            } else {
+                player.move(-500, 0)
+            }
         }
-    }
-})
-
-onCollide("bullet", "enemy", (bullet, enemy) => {
-    // add explosion here
-	destroy(bullet)
-	destroy(enemy)
-    play("explode")
-	score.text = "Score: " + (score.value += 10)
-
-    // spawn power ups
-    const randomize = rand(0, 100)
-    if(randomize < 5) {
-        spawnPowerUps()
-    }
-})
-
-onCollide("player", "enemy", () => {
-    player.hurt(1)
-    play("hurt")
-    life.text = "Life: " + player.hp()
-})
-
-onCollide("player", "power_ups", (_, power_ups) => {
-    const isHeart = power_ups.width == 64
-    if(isHeart) {
-        player.heal(1)
+        if (isKeyDown("s")) {
+            if(player.pos.y >= height() - player.height) {
+                player.move(0, 0)
+            } else {
+                player.move(0, 500)
+            }
+        }
+        if (isKeyDown("d")) {
+            if(player.pos.x >= width() - player.width) {
+                player.move(0, 0)
+            } else {
+                player.move(500, 0)
+            }
+        }
+    })
+    
+    onCollide("bullet", "enemy", (bullet, enemy) => {
+        // add explosion here
+        destroy(bullet)
+        destroy(enemy)
+        play("explode")
+        score.text = "Score: " + (score.value += 10)
+    
+        // spawn power ups
+        const randomize = rand(0, 100)
+        if(randomize < 5) {
+            spawnPowerUps()
+        }
+    })
+    
+    onCollide("player", "enemy", () => {
+        player.hurt(1)
+        play("hurt")
         life.text = "Life: " + player.hp()
-    } else {
-        // update fire rate
-    }
-    destroy(power_ups)
-})
+        if(player.hp() <= 0) {
+            goToScore()
+        }
+    })
+    
+    onCollide("player", "power_ups", (_, power_ups) => {
+        const isHeart = power_ups.width == 64
+        if(isHeart) {
+            player.heal(1)
+            life.text = "Life: " + player.hp()
+        } else {
+            // update fire rate
+        }
+        destroy(power_ups)
+    })
+    
+    onCollide("player", "enemy_bullet", (_, enemy_bullet) => {
+        player.hurt(1)
+        play("hurt")
+        life.text = "Life: " + player.hp()
+        destroy(enemy_bullet)
+        if(player.hp() <= 0) {
+            goToScore()
+        }
+    })
 
-onCollide("player", "enemy_bullet", (_, enemy_bullet) => {
-    player.hurt(1)
-    play("hurt")
-    life.text = "Life: " + player.hp()
-    destroy(enemy_bullet)
-    if(player.hp() == 0) {
+    function goToScore() {
+        const final_score = score.value
+        destroyAll("enemy")
+        destroyAll("bullet")
+        destroyAll("enemy_bullet")
+        destroyAll("power_ups")
         destroy(player)
+        destroy(score)
+        destroy(life)
+        isGameStart = false
+        scoreMenu(final_score)
     }
-})
+}
 
 // TO ADD:
 // -add explosion on enemy death
 // -background parallax
-// -power ups/bullet
+// -power ups bullet
 // -add dash
-// -add menu
